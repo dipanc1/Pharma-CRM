7e68122bb67092e537ee336133ced4025f031a35
dipanc1 <dipanchhabra@gmail.com>
2026-08-30 12:25:38 +0530

    fix: remove unused ArrowPathIcon import from Products.js

--- src/pages/products/Products/Products.js
+++ src/pages/products/Products/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PlusIcon, CubeIcon, PencilIcon, ArrowPathIcon, DocumentTextIcon } from '@heroicons/react/24/outline';
+import { PlusIcon, CubeIcon, PencilIcon, DocumentTextIcon } from '@heroicons/react/24/outline';
 import {
   AddButton,
   Header,
